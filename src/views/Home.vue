211465b6a065b28ce6e637f5e7a5465c72c1625d
JUMBO\dannychou <dannychou@jumbogames.com.tw>
2022-08-22 15:20:18 +0800

    feat: 請求數據攜帶token

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,22 @@
     <div>初始化頁面</div>
 </template>
 
-<script></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import axios from 'axios';
+
+onMounted(async () => {
+    try {
+        const data = await axios.get('/api/users/current', {
+            headers: {
+                Authorization: localStorage.token
+            }
+        });
+        console.log('data', data);
+    } catch (error) {
+        console.log(error);
+    }
+});
+</script>
 
 <style></style>
